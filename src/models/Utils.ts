65e3a2adfad066c2d503ae31ed12e2c9ea00ed50
snehil.bhushan <snehil.bhushan@akqa.com>
2024-01-21 15:47:12 +0000

    fix(keys): enhance sorting functionality for props with dependencies

--- src/models/Utils.ts
+++ src/models/Utils.ts
@@ -1,3 +1,4 @@
+import { arrValue } from '../plugin/Utils';
 import {
   PSComponentPropertyDefinitions,
   PSComponentPropertyItem,
@@ -14,15 +15,48 @@ export const propIsBooleanType = (
   compProp: PSComponentPropertyItems
 ): compProp is PSComponentPropertyItem => compProp.type === 'BOOLEAN';
 
-export function sortPropsOfCompPropDef(
+export const sortPropsOfCompPropDef = (
   compPropDef: PSComponentPropertyDefinitions
-) {
-  return Object.keys(compPropDef).sort((a, b) => {
-    const isInstance = (x) => (propIsInstanceType(compPropDef[x]) ? 2 : 0);
-    const isLinkedToLayer = (x) => (x.indexOf('#') !== -1 ? 1 : 0);
+): string[] => {
+  const generalSort = (keys = Object.keys(compPropDef)): string[] =>
+    keys.sort((a, b) => {
+      const isInstance = (x) => (propIsInstanceType(compPropDef[x]) ? 2 : 0);
+      const isLinkedToLayer = (x) => (x.indexOf('#') !== -1 ? 1 : 0);
 
-    const allSortFunc = (x) => isInstance(x) + isLinkedToLayer(x);
+      const allSortFunc = (x) => isInstance(x) + isLinkedToLayer(x);
 
-    return allSortFunc(a) - allSortFunc(b);
-  });
-}
+      return allSortFunc(a) - allSortFunc(b);
+    });
+
+  function sortKeysByDependency() {
+    const keys = Object.keys(compPropDef);
+    let sortedKeys = [];
+
+    // First pass: add all keys without disabledByProperty
+    keys.forEach((key) => {
+      if (!arrValue(compPropDef[key].disabledByProperty)) {
+        sortedKeys.push(key);
+      }
+    });
+    sortedKeys = generalSort(sortedKeys)
+
+
+    // Second pass: insert keys with disabledByProperty after the location of dependency key
+    keys.forEach((key) => {
+      if (compPropDef[key].disabledByProperty) {
+        const dependencies = compPropDef[key].disabledByProperty;
+        let maxIndex = -1;
+        dependencies.forEach((dependency) => {
+          const index = sortedKeys.indexOf(dependency);
+          if (index > maxIndex) {
+            maxIndex = index;
+          }
+        });
+        sortedKeys.splice(maxIndex + 1, 0, key);
+      }
+    });
+    return sortedKeys;
+  }
+
+  return sortKeysByDependency();
+};
--- src/plugin/Utils.ts
+++ src/plugin/Utils.ts
@@ -8,3 +8,14 @@ export function objValue<T extends Record<string, unknown>>(
   const exists = !!(item && Object.keys(item).length);
   return exists ? item : undefined;
 }
+
+/**
+ * Returns object is there are keys inside it. Returns undefined if obj = null | {}
+ * @param item
+ */
+export function arrValue<T extends Array<unknown>>(
+  item?: T,
+): T | undefined {
+  const exists = !!(item && item.length);
+  return exists ? item : undefined;
+}
